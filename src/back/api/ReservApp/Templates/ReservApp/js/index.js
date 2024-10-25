897de61f15f0bf48298a462cf1f33a0e2b4abcb7
Christian Gutman <cgutman@tecnosoftware.com>
2024-10-25 09:06:38 -0300

    scripts js widgets test

--- src/back/api/ReservApp/Templates/ReservApp/js/index.js
+++ src/back/api/ReservApp/Templates/ReservApp/js/index.js
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import Main from "./pages/main/main";
-
+import './index.css'
+//import ReactDOM from 'react-dom'
+//ReactDOM.render( <App />, document.getElementById('root'))
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
 root.render(
   <StrictMode>
     <Main />
