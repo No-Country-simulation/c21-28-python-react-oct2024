bcf7fb173f75dcdad0a9d265882957b052f399ed
Christian Gutman <cgutman@tecnosoftware.com>
2024-10-25 15:11:32 -0300

    scripts js widgets test

--- src/back/api/ReservApp/static/js/widgets/countdown/index.js
+++ src/back/api/ReservApp/static/js/widgets/countdown/index.js
@@ -1,71 +1,98 @@
-"use strict";
-import React, { useState, useEffect } from "react";
-import "./index.css";
+import React from 'react';
+import DateTimeDisplay from './DateTimeDisplay';
+import { useCountdown } from './hooks/useCountdown';
+import { useEffect, useState } from 'react';
 
-class CountDownTimer extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+/*********************************************/
 
-    render() { 
-        return (
-            <div className="countdown-body">
-                <div className="countdown-app">
-                    <div className="countdown-elementcontainer">
-                        <div className="countdown-timeparent">
-                            <div className="countdown-timecontainer title">
-                                <span className="countdown-time">
-                                    <div>
-                                        <p>Tiempo Restante :</p>
-                                        <p>{`${hours}h ${minutes}m ${seconds}s`}</p>
-                                    </div>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-customElements.define('countdown', CountDownTimer);
-
-
-const CountdownTimer = () => {
-    // Initial time in seconds (1 hour)
-    const initialTime = 60 * 60;
-    const [timeRemaining, setTimeRemaining] = useState(initialTime);
-
-    useEffect(() => {
-        const timerInterval = setInterval(() => {
-            setTimeRemaining((prevTime) => {
-                if (prevTime === 0) {
-                    clearInterval(timerInterval);
-                    // Perform actions when the timer reaches zero
-                    console.log('Countdown complete!');
-                    return 0;
-                } else {
-                    return prevTime - 1;
-                }
-            });
-        }, 1000);
-
-        // Cleanup the interval when the component unmounts
-        return () => clearInterval(timerInterval);
-    }, []); // The empty dependency array ensures the effect runs only once on mount
-
-    // Convert seconds to hours, minutes, and seconds
-    const hours = Math.floor(timeRemaining / 3600);
-    const minutes = Math.floor((timeRemaining % 3600) / 60);
-    const seconds = timeRemaining % 60;
-
-    return (
-        <div>
-            <p>Tiempo Restante :</p>
-            <p>{`${hours}h ${minutes}m ${seconds}s`}</p>
-        </div>
-    );
+const ExpiredNotice = () => {
+  return (
+    <div className="expired-notice">
+      <span>Expired!!!</span>
+      <p>Please select a future date and time.</p>
+    </div>
+  );
 };
 
-export default CountdownTimer;
+const ShowCounter = ({ days, hours, minutes, seconds }) => {
+  return (
+    <div className="show-counter">
+      <a
+        href="https://tapasadhikary.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="countdown-link"
+      >
+        <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
+        <p>:</p>
+        <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
+        <p>:</p>
+        <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
+      </a>
+    </div>
+  );
+};
+
+const CountDownTimer = ({ targetDate }) => {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+    return (
+      <ShowCounter
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+      />
+    );
+  }
+};
+/*********************************************/
+
+const DateTimeDisplay = ({ value, type, isDanger }) => {
+  return (
+    <div className={isDanger ? 'countdown danger' : 'countdown'}>
+      <p>{value}</p>
+      <span>{type}</span>
+    </div>
+  );
+};
+
+/*********************************************/
+
+const useCountDown = (targetDate) => {
+  const countDownDate = new Date(targetDate).getTime();
+
+  const [countDown, setCountDown] = useState(
+    countDownDate - new Date().getTime()
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(countDownDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+
+  return getReturnValues(countDown);
+};
+
+const getReturnValues = (countDown) => {
+  // calculate time left
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  return [days, hours, minutes, seconds];
+};
+
+
+export {CountDownTimer, useCountDown , DateTimeDisplay}
